20cdfc8247a210e5846e934d025a82dcf8897293
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2025-04-27 12:08:18 +0700

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-// popup.js
-window.onload = function() {
-  document.getElementById("welcomePopup").style.display = "flex";
-};
-
-function closePopup() {
-  document.getElementById("welcomePopup").style.display = "none";
-}
