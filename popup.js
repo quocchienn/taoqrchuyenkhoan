9585518bf349ea446f02750d764d2b458102c5cd
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2025-04-27 11:28:11 +0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,8 @@
+// popup.js
+window.onload = function() {
+  document.getElementById("welcomePopup").style.display = "flex";
+};
+
+function closePopup() {
+  document.getElementById("welcomePopup").style.display = "none";
+}
